ddddab82fc397bda34c53d9784b25ad19e6c22fa
Mustafa Bülbül <bulbulmustafa422@gmail.com>
2025-11-04 11:58:21 +0300

    add

--- 029.cpp
+++ 029.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int main()
-{
+class Temel {
+public:
+virtual void yaz() {		// virtual kaldır tekrar dene
+        cout << "Temel Fonksiyon" << endl;
+    }
+};
 
-	return 0;
+class Turetilmis : public Temel {
+public:
+    void yaz() {
+        cout << "Turetilmis Fonksiyon" << endl;
+    }
+
+public:
+
+};
+
+int main() {
+    Turetilmis T1;
+    Temel* ptr = &T1;
+    ptr->yaz();
+
+    return (0);
 }
--- 030.cpp
+++ 030.cpp
@@ -3,8 +3,38 @@
 
 using namespace std;
 
+class Hayvan {
+public:
+	virtual void sesCikar() {
+		cout << "Hayvan ses cikariyor" << endl;
+	}
+};
+
+class Kopek : public Hayvan {
+public:
+	void sesCikar() override {
+		cout << "Hav hav!" << endl;
+	}
+};
+
+class Kedi : public Hayvan {
+public:
+	void sesCikar() override {
+		cout << "Miyav miyav!" << endl;
+	}
+};
+
 int main()
 {
+	Hayvan* hayvanPtr;
+	Kopek kopekObj;
+	Kedi kediObj;
+
+	hayvanPtr = &kopekObj;
+	hayvanPtr->sesCikar();
+
+	hayvanPtr = &kediObj;
+	hayvanPtr->sesCikar();
 
 	return 0;
 }
