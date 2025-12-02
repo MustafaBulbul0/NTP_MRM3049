f3240f01a3e7187c765e43331c55d5a3e726d70b
Mustafa Bülbül <bulbulmustafa422@gmail.com>
2025-12-02 09:41:19 +0300

    add

--- 044.cpp
+++ 044.cpp
@@ -3,8 +3,42 @@
 
 using namespace std;
 
+class person {
+	public:
+		person() {
+			cout << "Person constructor called." << endl;
+		}
+		void yuru(){
+			cout << "Person is walking." << endl;
+		}
+};
+
+class ogrenci: virtual public person {  // virtual yokken hata veriyor
+	public:
+		ogrenci() {
+			cout << "Ogrenci constructor called." << endl;
+		}
+};
+
+class calisan: virtual public person {	// virtual yokken hata veriyor
+	public:
+		calisan() {
+			cout << "Calisan constructor called." << endl;
+		}
+};
+
+class yariZamanliOgrenci: public ogrenci, public calisan {
+	public:
+		yariZamanliOgrenci() {
+			cout << "YariZamanliOgrenci constructor called." << endl;
+		}
+};
+
 int main()
 {
+	yariZamanliOgrenci yzo;
+
+	yzo.yuru();
 
 	return 0;
 }
--- 045.cpp
+++ 045.cpp
@@ -3,8 +3,40 @@
 
 using namespace std;
 
+class sekil {
+	public:
+		virtual void ciz() = 0;
+
+		virtual ~sekil() {}
+
+		void bilgi() {
+			cout << "Bu bir sekildir." << endl;
+		}
+};
+
+class daire : public sekil {
+	public:
+		void ciz() override {
+			cout << "Daire ciziliyor..." << endl;
+		}
+};
+
+class kare : public sekil {
+	public:
+		void ciz() override {
+			cout << "Kare ciziliyor..." << endl;
+		}
+};
+
 int main()
 {
+	sekil* sekiller[] = {new daire(), new kare()};
+
+	for (sekil* s : sekiller) {
+		s->ciz();
+		s->bilgi();
+		delete s;
+	}
 
 	return 0;
 }
